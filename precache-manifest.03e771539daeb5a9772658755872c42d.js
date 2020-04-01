1fdec879c66f2c8d554d6ae152f84843c1c12830
rodneyslafuente <rodneyslafuente@gmail.com>
2020-04-01 10:53:01 -0400

    Updates

---
+++ precache-manifest.03e771539daeb5a9772658755872c42d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd68103bbebeb4b34a59549d4a9862b7",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "7ea795a80bf84aca43c3",
+    "url": "/personal-assistant/static/js/2.a98cfc8b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/personal-assistant/static/js/2.a98cfc8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "104243ba01f810f658aa",
+    "url": "/personal-assistant/static/js/main.20211987.chunk.js"
+  },
+  {
+    "revision": "6af68dc79e5ea81f8987",
+    "url": "/personal-assistant/static/js/runtime-main.9f1a1dcf.js"
+  }
+]);
